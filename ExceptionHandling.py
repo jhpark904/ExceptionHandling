d0f2d462a2f1cf8bed3c5d7e87e4a9201dc3e599
jhpark904 <60660722+jhpark904@users.noreply.github.com>
2020-08-04 17:50:02 -0400

    Add files via upload

---
+++ ExceptionHandling.py
@@ -0,0 +1,20 @@
+# PROGRAMMING ASSIGNMENT 07
+# Filename: 'exercise2.py'
+#
+# See instructions in pdf file
+#
+
+def main():
+    """The program does the following:
+    1) asks the user to input a filename (supposedly in the same folder as this program)
+	2) reads the numbers (type int) in the file (filename given by the user in previous step) and stores them into a list
+	3) prints the minimum, the maximum and the average of the numbers (2 decimal places for the average)
+    
+    Note: your program should be impossible to crash. Think about all the possibilities to make your program crash.
+    If one of the steps throws an exception, display a message to the user and your program should loop back to step 1."""
+    #WRITE YOUR PROGRAM HERE
+    
+    
+
+#Call the main() function
+main()
